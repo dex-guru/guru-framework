8eed20f5c2c4d98511c9170604718ef7b0a9d0bc
Alexey <alex.volynshchikov@techusage.com>
2024-10-09 09:33:01 +0700

    fix env

--- external_workers/testnet_arbitrage/uniswap_v3_execue_swap.py
+++ external_workers/testnet_arbitrage/uniswap_v3_execue_swap.py
@@ -15,11 +15,11 @@ TOPIC = 'executeSwapUniswapV3'
 VARIABLE_NOT_FOUND_ERR_CODE = "VARIABLE_NOT_FOUND"
 VARIABLE_NOT_FOUND_MSG = "Variable '{}' not found"
 
-WEB3_PK = os.environ.get('WEB3_PK', {'0x0000000000000000000000000000000000000000':
-                                     '0000000000000000000000000000000000000000000000000000000000000000'})
+WEB3_PK = os.environ.get(
+    'WEB3_PK',
+    '{"0x0000000000000000000000000000000000000000":"0000000000000000000000000000000000000000000000000000000000000000"}')
 
-if isinstance(WEB3_PK, str):
-    WEB3_PK = json.loads(WEB3_PK)
+WEB3_PK = json.loads(WEB3_PK)
 
 
 with open(Path(__file__).parent / 'uniswap_v3_router_abi.json') as f:
